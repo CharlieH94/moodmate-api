4c71bc1de366b8af35c08c22f0e43ae2797af975
Jake Samuels <samuels242@googlemail.com>
2023-03-27 21:03:36 +0100

    Add signup route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const userController = require("./../controllers/userControllers");
+const authController = require("./../controllers/authController");
 
 const router = express.Router();
 
+router.route("/signup").post(authController.signup);
+
 router.route("/").get(userController.getAllUsers);
+
 router
   .route("/:userId")
   .get(userController.getUserById)
