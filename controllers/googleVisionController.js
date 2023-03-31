97271edaadb64853568f59d73b4fbae3a160ba0c
Jake Samuels <samuels242@googlemail.com>
2023-03-31 11:49:47 +0100

    fix url issue

--- controllers/googleVisionController.js
+++ controllers/googleVisionController.js
@@ -1,10 +1,8 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
-const apiURL = process.env.API_URL.replace(
-  "<API_KEY>",
-  process.env.REACT_APP_API_KEY
-);
+const apiURL = process.env.API_URL;
+console.log(apiURL);
 
 exports.googleApiURL = (req, res) => {
   res.send(apiURL);
