a248a45d637fd6f7cb8e817850f1cff666f4915d
Jake Samuels <samuels242@googlemail.com>
2023-03-31 11:42:59 +0100

    Fix .env issue

--- controllers/googleVisionController.js
+++ controllers/googleVisionController.js
@@ -1,4 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const apiURL = process.env.API_URL;
+
 exports.googleApiURL = (req, res) => {
-  const apiURL = process.env.API_URL;
   res.send(apiURL);
 };
