2ec41ac07352ed8dd0db1035ab6a7c508bd4405f
Jake Samuels <samuels242@googlemail.com>
2023-03-31 10:47:22 +0100

    Fix url replace

--- utils/googleApi.js
+++ utils/googleApi.js
@@ -3,7 +3,10 @@ const router = express.Router();
 require("dotenv").config();
 
 router.get("/api-url", (req, res) => {
-  const apiURL = process.env.API_URL;
+  const apiURL = process.env.API_URL.replace(
+    "<API_KEY>",
+    process.env.REACT_APP_API_KEY
+  );
   res.send(apiURL);
 });
 
