9c662f9c29474575ae5661b093e61573d7e27f9a
Jake Samuels <samuels242@googlemail.com>
2023-03-31 12:09:36 +0100

    add env variables to render

--- controllers/googleVisionController.js
+++ controllers/googleVisionController.js
@@ -1,6 +1,3 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-
 exports.googleApiURL = (req, res) => {
   const apiURL = process.env.API_URL;
   res.status(200).json({
