67d9092c7d02760abeef637c1e213bfa1f7e60b8
Jake Samuels <samuels242@googlemail.com>
2023-03-31 11:46:05 +0100

    replace placeholder

--- controllers/googleVisionController.js
+++ controllers/googleVisionController.js
@@ -1,7 +1,10 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
-const apiURL = process.env.API_URL;
+const apiURL = process.env.API_URL.replace(
+  "<API_KEY>",
+  process.env.REACT_APP_API_KEY
+);
 
 exports.googleApiURL = (req, res) => {
   res.send(apiURL);
